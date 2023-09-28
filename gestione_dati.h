9931d9c93acd46dfc637cf1fffcc783d765e4539
jack <gcunardi@gmail.com>
2023-09-28 16:08:28 +0200

    server.c completato

---
+++ gestione_dati.h
@@ -0,0 +1,8 @@
+//
+// Created by Giacomo Cunardi on 28/09/23.
+//
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include "UDP.h"
--- server.c
+++ server.c
@@ -1,3 +1,71 @@
-//
 // Created by Giacomo Cunardi on 28/09/23.
 //
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "UDP.h"
+
+#define UDP_PORT 23365
+
+int ONOFF(unsigned char buffer[1024]) // 0 = OK 1 = ERR (La funzione non è completa ma simula il funzionanamnto della lampadina)
+{
+    sleep(atoi((const char *) buffer[1]));
+    return rand()%2;
+}
+
+int main(void)
+{
+    unsigned char buffer[1024];     // Reception buffer
+    unsigned long ip_address;       // Variable to store the client's IP address
+    unsigned short port_number;     // Variable to store the client's port number
+    int n;
+    short iSwitch = 0;
+
+    for(n = 0; n < 1024; n++)
+    {
+        buffer[n] = 0;
+    }
+
+    if (UDP_init(UDP_PORT) < 0) // Initialize the socket with UDP port number 54321
+    {
+        printf("ERR 101: Socket Error\r\n");
+        return -1;
+    }
+
+
+    printf("Service Activated\r\n");
+
+
+    for (int i = 0; i < 2; ++i)
+    {
+        if (iSwitch == 0)
+        {
+            while (1)
+            {
+                if (( UDP_receive(&ip_address, &port_number, buffer, sizeof(buffer))) > 0) // Receive a datagram and check the message
+                {
+                    if (ONOFF(buffer) == 1)
+                    {
+                        printf("ERR 102: Object error\n\r");
+                        buffer[1023] = 1;
+                        break;
+                    }
+
+                    UDP_send(ip_address, port_number, (void*)&buffer, sizeof(buffer)); // Send the datagram back to the client
+                }
+            }
+            iSwitch = 1;
+        }
+
+        else
+        {
+            UDP_close();
+            return 0;
+        }
+
+
+    }
+
+}
