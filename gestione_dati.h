4fb001a7d8980b3a14d27624d40d3ff5b54d2ee9
jack <gcunardi@gmail.com>
2023-09-28 21:57:47 +0200

    ChatGPT and Research version

--- gestione_dati.h
+++ gestione_dati.h
@@ -1,25 +1,33 @@
-
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <strings.h>
 
-struct dati{
-    char on_of[3];
+enum State { OFF, ON };
+
+struct dati {
+    enum State on_off;
     int time;
-}data;
+} data;
 
-int gestOF(struct dati data){
-    if(strcmp(data.on_of, "off")==0){
-        return -1; //in caso di off
+#define OFF_STR "off"
+#define ON_STR "on"
+
+// Function to convert enum State to string representation
+const char* stateToString(enum State state) {
+    return (state == OFF) ? OFF_STR : ON_STR;
+}
+
+int gestOF(struct dati data) {
+    const char* stateStr = stateToString(data.on_off);
+
+    if (strcasecmp(stateStr, OFF_STR) == 0) {
+        return -1; // in caso di off
     }
-    if(strcmp(data.on_of, "OFF")==0){
-        return -1; //in caso di off
+    if (strcasecmp(stateStr, ON_STR) == 0) {
+        return 0; // in caso di on
     }
-    if(strcmp(data.on_of, "ON")==0){
-        return 0; //in caso di on
-    }
-    if(strcmp(data.on_of, "on")==0){
-        return 0; //in caso di on
-    }
+    // Handle other cases or return an error code here.
+    return -2; // Unknown state
 }
